feb418d657f5b6d857537ceb69e7b98686855d97
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-11 10:24:52 +0000

    V1.0.0:chenghui: 逆向生成模快调整

--- code/ecs/ecs-generator/src/main/java/com/util/GenConstants.java
+++ code/ecs/ecs-generator/src/main/java/com/util/GenConstants.java
@@ -2,4 +2,8 @@ package com.util;
 
 public class GenConstants {
 	
+	public static  final String  db_driver="com.mysql.jdbc.Driver";
+	public static  final String  db_name="";
+	public static  final String  db_password="";
+	public static  final String  db_URL="jdbc:mysql://10.120.24.75:3306/btcp";
 }
