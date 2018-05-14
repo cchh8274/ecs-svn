94e9ce66be4330539f4fbe4ebc2e14d63153da9e
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-14 09:15:27 +0000

    V1.0:zhaohao:大学实体类

--- code/ecs/ecs-dao/src/main/java/cn/kanmars/entity/TblUniversityInfo.java
+++ code/ecs/ecs-dao/src/main/java/cn/kanmars/entity/TblUniversityInfo.java
@@ -35,7 +35,7 @@ public class TblUniversityInfo implements java.io.Serializable{
 
     /**
      * 点的位置
-     * tbl_university_info.backImg
+     * tbl_university_info.backimg
      */
     private String backimg;
 
@@ -67,6 +67,7 @@ public class TblUniversityInfo implements java.io.Serializable{
     private String zme;
     private String myyjsr;
     private String rzstase;
+    private String headimg;
 	public String getId() {
 		return id;
 	}
@@ -91,10 +92,10 @@ public class TblUniversityInfo implements java.io.Serializable{
 	public void setAreaId(String areaId) {
 		this.areaId = areaId;
 	}
-	public String getBackimg() {
+	public String getbackimg() {
 		return backimg;
 	}
-	public void setBackimg(String backimg) {
+	public void setbackimg(String backimg) {
 		this.backimg = backimg;
 	}
 	public String getCreateTime() {
@@ -151,9 +152,19 @@ public class TblUniversityInfo implements java.io.Serializable{
 	public void setRzstase(String rzstase) {
 		this.rzstase = rzstase;
 	}
-	public TblUniversityInfo(String id, String univerName, String areaName, String areaId, String backImg,
+	public String getHeadimg() {
+		return headimg;
+	}
+	public void setHeadimg(String headimg) {
+		this.headimg = headimg;
+	}
+	public TblUniversityInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public TblUniversityInfo(String id, String univerName, String areaName, String areaId, String backimg,
 			String createTime, String createUser, String upTime, String upUser, String code, String sy_me, String zme,
-			String myyjsr, String rzstase) {
+			String myyjsr, String rzstase, String headimg) {
 		super();
 		this.id = id;
 		this.univerName = univerName;
@@ -169,197 +180,18 @@ public class TblUniversityInfo implements java.io.Serializable{
 		this.zme = zme;
 		this.myyjsr = myyjsr;
 		this.rzstase = rzstase;
-	}
-	public TblUniversityInfo() {
-		super();
-		// TODO Auto-generated constructor stub
+		this.headimg = headimg;
 	}
 	@Override
 	public String toString() {
 		return "TblUniversityInfo [id=" + id + ", univerName=" + univerName + ", areaName=" + areaName + ", areaId="
-				+ areaId + ", backImg=" + backimg + ", createTime=" + createTime + ", createUser=" + createUser
+				+ areaId + ", backimg=" + backimg + ", createTime=" + createTime + ", createUser=" + createUser
 				+ ", upTime=" + upTime + ", upUser=" + upUser + ", code=" + code + ", sy_me=" + sy_me + ", zme=" + zme
-				+ ", myyjsr=" + myyjsr + ", rzstase=" + rzstase + "]";
+				+ ", myyjsr=" + myyjsr + ", rzstase=" + rzstase + ", headimg=" + headimg + "]";
 	}
-    
-    
-  
-	
-
-   private Integer limitStart;
-   public Integer getLimitStart(){
-       return limitStart;
-   }
-   public void setLimitStart(Integer limitStart){
-       this.limitStart = limitStart;
-   }
-   private Integer limitSize;
-   public Integer getLimitSize(){
-       return limitSize;
-   }
-   public void setLimitSize(Integer limitSize){
-       this.limitSize = limitSize;
-   }
-    
-   /* public TblUniversityInfo(){super();}
-    public TblUniversityInfo(String id,String univerName,String areaName,String areaId,String backImg,String createTime,String createUser,String upTime,String upUser) {
-        this.id = id;
-        this.univerName = univerName;
-        this.areaName = areaName;
-        this.areaId = areaId;
-        this.backImg = backImg;
-        this.createTime = createTime;
-        this.createUser = createUser;
-        this.upTime = upTime;
-        this.upUser = upUser;
-    }
-    *//**
-     * ID
-     * tbl_university_info.id
-     *//*
-    public String getId(){
-        return id;
-    }
-
-    *//**
-     * ID
-     * tbl_university_info.id
-     *//*
-    public void setId(String id){
-        this.id=id;
-    }
-
-    *//**
-     * 大学名称
-     * tbl_university_info.univer_name
-     *//*
-    public String getUniverName(){
-        return univerName;
-    }
-
-    *//**
-     * 大学名称
-     * tbl_university_info.univer_name
-     *//*
-    public void setUniverName(String univerName){
-        this.univerName=univerName;
-    }
-
-    *//**
-     * 区域名称
-     * tbl_university_info.area_name
-     *//*
-    public String getAreaName(){
-        return areaName;
-    }
-
-    *//**
-     * 区域名称
-     * tbl_university_info.area_name
-     *//*
-    public void setAreaName(String areaName){
-        this.areaName=areaName;
-    }
-
-    *//**
-     * 区域ID
-     * tbl_university_info.area_id
-     *//*
-    public String getAreaId(){
-        return areaId;
-    }
-
-    *//**
-     * 区域ID
-     * tbl_university_info.area_id
-     *//*
-    public void setAreaId(String areaId){
-        this.areaId=areaId;
-    }
-
-    *//**
-     * 点的位置
-     * tbl_university_info.backImg
-     *//*
-    public String getbackImg(){
-        return backImg;
-    }
-
-    *//**
-     * 点的位置
-     * tbl_university_info.backImg
-     *//*
-    public void setbackImg(String backImg){
-        this.backImg=backImg;
-    }
-
-    *//**
-     * 创建时间
-     * tbl_university_info.create_time
-     *//*
-    public String getCreateTime(){
-        return createTime;
-    }
-
-    *//**
-     * 创建时间
-     * tbl_university_info.create_time
-     *//*
-    public void setCreateTime(String createTime){
-        this.createTime=createTime;
-    }
-
-    *//**
-     * 创建人
-     * tbl_university_info.create_user
-     *//*
-    public String getCreateUser(){
-        return createUser;
-    }
-
-    *//**
-     * 创建人
-     * tbl_university_info.create_user
-     *//*
-    public void setCreateUser(String createUser){
-        this.createUser=createUser;
-    }
-
-    *//**
-     * 修改时间
-     * tbl_university_info.up_time
-     *//*
-    public String getUpTime(){
-        return upTime;
-    }
-
-    *//**
-     * 修改时间
-     * tbl_university_info.up_time
-     *//*
-    public void setUpTime(String upTime){
-        this.upTime=upTime;
-    }
-
-    *//**
-     * 修改人
-     * tbl_university_info.up_user
-     *//*
-    public String getUpUser(){
-        return upUser;
-    }
-
-    *//**
-     * 修改人
-     * tbl_university_info.up_user
-     *//*
-    public void setUpUser(String upUser){
-        this.upUser=upUser;
-    }
-
-    *//**
+	/**
      * 自定义列，分页查询用
-     *//*
+     */
     private Integer limitStart;
     public Integer getLimitStart(){
         return limitStart;
@@ -374,13 +206,7 @@ public class TblUniversityInfo implements java.io.Serializable{
     public void setLimitSize(Integer limitSize){
         this.limitSize = limitSize;
     }
-	@Override
-	public String toString() {
-		return "TblUniversityInfo [id=" + id + ", univerName=" + univerName + ", areaName=" + areaName + ", areaId="
-				+ areaId + ", backImg=" + backImg + ", createTime=" + createTime + ", createUser=" + createUser
-				+ ", upTime=" + upTime + ", upUser=" + upUser + ", limitStart=" + limitStart + ", limitSize="
-				+ limitSize + "]";
-	}*/
-    
+	
+
 }
 
