ada9cfc396810e0720da99b699fe023dcdc89375
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-16 02:12:31 +0000

    v1.0:zhaohao:图片上传工具类

---
+++ ecs-bean/src/main/java/com/ycb/util/ConstantsBean.java
@@ -0,0 +1,9 @@
+package com.ycb.util;
+
+public class ConstantsBean {
+
+	 public static String IMG_PATH ="D:/ider_gzkj/apache-tomcat-8.0.0/webapps/ROOT/";
+
+	 public static String IMG_SERVER_PATH ="http://127.0.0.1:8079/";
+
+}
