0ba3da71c76bbdb6d5b1e95cb2069498e1feb34f
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-17 08:02:25 +0000

    v1.0:zhaohao:用户意向表后台系统提交

--- code/ecs/ecs-dao/src/main/java/cn/kanmars/entity/TblWxUserIntention.java
+++ code/ecs/ecs-dao/src/main/java/cn/kanmars/entity/TblWxUserIntention.java
@@ -1,83 +1,150 @@
 package cn.kanmars.entity;
-
-public class TblWxUserIntention {
+/**
+ * @author xzkp
+ *	用户意向表
+ */
+public class TblWxUserIntention implements java.io.Serializable{
     private String id;
 
+    /**
+     * 用户id
+     */
     private String openid;
 
+    /**
+     * 用户昵称
+     */
     private String nickname;
 
+    /**
+     * 用户手机号码
+     */
     private String userCell;
 
+    /**
+     * 大学id
+     */
     private String universityId;
 
+    /**
+     * 大学名称
+     */
     private String universityName;
 
+    /**
+     * 创建时间
+     */
     private String creationTime;
 
+    /**
+     * 跟进记录
+     */
     private String followRecord;
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getOpenid() {
-        return openid;
-    }
+	public String getOpenid() {
+		return openid;
+	}
 
-    public void setOpenid(String openid) {
-        this.openid = openid == null ? null : openid.trim();
-    }
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
 
-    public String getNickname() {
-        return nickname;
-    }
+	public String getNickname() {
+		return nickname;
+	}
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname == null ? null : nickname.trim();
-    }
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
 
-    public String getUserCell() {
-        return userCell;
-    }
+	public String getUserCell() {
+		return userCell;
+	}
 
-    public void setUserCell(String userCell) {
-        this.userCell = userCell == null ? null : userCell.trim();
-    }
+	public void setUserCell(String userCell) {
+		this.userCell = userCell;
+	}
 
-    public String getUniversityId() {
-        return universityId;
-    }
+	public String getUniversityId() {
+		return universityId;
+	}
 
-    public void setUniversityId(String universityId) {
-        this.universityId = universityId == null ? null : universityId.trim();
-    }
+	public void setUniversityId(String universityId) {
+		this.universityId = universityId;
+	}
 
-    public String getUniversityName() {
-        return universityName;
-    }
+	public String getUniversityName() {
+		return universityName;
+	}
 
-    public void setUniversityName(String universityName) {
-        this.universityName = universityName == null ? null : universityName.trim();
-    }
+	public void setUniversityName(String universityName) {
+		this.universityName = universityName;
+	}
 
-    public String getCreationTime() {
-        return creationTime;
-    }
+	public String getCreationTime() {
+		return creationTime;
+	}
 
-    public void setCreationTime(String creationTime) {
-        this.creationTime = creationTime == null ? null : creationTime.trim();
-    }
+	public void setCreationTime(String creationTime) {
+		this.creationTime = creationTime;
+	}
 
-    public String getFollowRecord() {
-        return followRecord;
-    }
+	public String getFollowRecord() {
+		return followRecord;
+	}
 
-    public void setFollowRecord(String followRecord) {
-        this.followRecord = followRecord == null ? null : followRecord.trim();
+	public void setFollowRecord(String followRecord) {
+		this.followRecord = followRecord;
+	}
+
+	public TblWxUserIntention() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TblWxUserIntention(String id, String openid, String nickname, String userCell, String universityId,
+			String universityName, String creationTime, String followRecord) {
+		super();
+		this.id = id;
+		this.openid = openid;
+		this.nickname = nickname;
+		this.userCell = userCell;
+		this.universityId = universityId;
+		this.universityName = universityName;
+		this.creationTime = creationTime;
+		this.followRecord = followRecord;
+	}
+
+	@Override
+	public String toString() {
+		return "TblWxUserIntention [id=" + id + ", openid=" + openid + ", nickname=" + nickname + ", userCell="
+				+ userCell + ", universityId=" + universityId + ", universityName=" + universityName + ", creationTime="
+				+ creationTime + ", followRecord=" + followRecord + "]";
+	}
+
+	/**
+     * 自定义列，分页查询用
+     */
+    private Integer limitStart;
+    public Integer getLimitStart(){
+        return limitStart;
+    }
+    public void setLimitStart(Integer limitStart){
+        this.limitStart = limitStart;
+    }
+    private Integer limitSize;
+    public Integer getLimitSize(){
+        return limitSize;
+    }
+    public void setLimitSize(Integer limitSize){
+        this.limitSize = limitSize;
     }
 }
