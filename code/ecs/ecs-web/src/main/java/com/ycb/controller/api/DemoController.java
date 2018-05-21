9460a7506b83819dafac5aec2fd239e761fdeba9
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-21 06:59:25 +0000

    V1.0.0:chenghui: 配置文件

--- code/ecs/ecs-web/src/main/java/com/ycb/controller/api/DemoController.java
+++ code/ecs/ecs-web/src/main/java/com/ycb/controller/api/DemoController.java
@@ -3,7 +3,12 @@ package com.ycb.controller.api;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.ycb.base.BaseController;
+import com.ycb.util.WxSessionUtils;
 
 /**
  * 所有的API 接口 都要继承 BaseContoller
@@ -12,14 +17,17 @@ import com.ycb.base.BaseController;
  * @author xzkp
  *
  */
+@Controller
+@RequestMapping("/demo")
 public class DemoController  extends BaseController {
 		
-	
+	@RequestMapping("/de")
+	@ResponseBody
 	public String  apiA(String  jsonStr){
 		Map<String,String>  map=new HashMap();
 		//1. DATA  =  LIST   toJSONString(LIST);查询类的 直接数据
 		//2. DATA String  ==> message:'新增成功'; 操作类的  返回 Key Value
-		map.put("messager", "新增");
+		map.put("messager", WxSessionUtils.getToken());
 		return this.toJSONString(map);
 		
 	}
