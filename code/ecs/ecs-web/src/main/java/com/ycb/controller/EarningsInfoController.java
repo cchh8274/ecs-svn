bdcce5414c938dc3a1f4e914969d0548c81a8af6
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-22 07:47:56 +0000

    V1.0:赵浩：收益明细修改

--- code/ecs/ecs-web/src/main/java/com/ycb/controller/EarningsInfoController.java
+++ code/ecs/ecs-web/src/main/java/com/ycb/controller/EarningsInfoController.java
@@ -16,6 +16,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.ycb.base.BaseController;
 import com.ycb.service.AmountInfoService;
 import com.ycb.service.EarningsGatherService;
@@ -115,9 +117,9 @@ public class EarningsInfoController extends BaseController{
 	@ResponseBody
 	public String shouyimingxi(String jsonStr){
 		try {
-			//JSONObject json = JSON.parseObject(jsonStr);
-			//String openid = json.getString("openid");
-			String openid = jsonStr;
+			JSONObject json = JSON.parseObject(jsonStr);
+			String openid = json.getString("openid");
+			//String openid = jsonStr;
 	 String rikai = DateUtils.getBeforeDaybb()+" 00:00:00"; 
      String rijie = DateUtils.getBeforeDaybb()+" 23:59:59";
 	List<TblEarningsInfo>  ri=earningsInfoService.shouyimingxiyueri(openid,rikai,rijie);
