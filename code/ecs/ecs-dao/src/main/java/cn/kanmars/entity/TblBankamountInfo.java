4a87f961fab31535730ab32baefcb84fefb0c911
root <root@189bd6b8-9ba3-4e08-8d5f-42fdea2cfcbe>
2018-05-22 08:03:15 +0000

    V1.0:赵浩：银行帐户表新增提现密码字段

--- code/ecs/ecs-dao/src/main/java/cn/kanmars/entity/TblBankamountInfo.java
+++ code/ecs/ecs-dao/src/main/java/cn/kanmars/entity/TblBankamountInfo.java
@@ -86,231 +86,12 @@ public class TblBankamountInfo implements java.io.Serializable{
      * tbl_bankamount_info.update_user
      */
     private String updateUser;
-
-
-    public TblBankamountInfo(){super();}
-    public TblBankamountInfo(String id,String userName,String idcard,String phone,String wxName,String openid,String bankAmountNo,String bankName,String bankAdress,String createTime,String createUser,String updateTime,String updateUser) {
-        this.id = id;
-        this.userName = userName;
-        this.idcard = idcard;
-        this.phone = phone;
-        this.wxName = wxName;
-        this.openid = openid;
-        this.bankAmountNo = bankAmountNo;
-        this.bankName = bankName;
-        this.bankAdress = bankAdress;
-        this.createTime = createTime;
-        this.createUser = createUser;
-        this.updateTime = updateTime;
-        this.updateUser = updateUser;
-    }
+    
     /**
-     * id
-     * tbl_bankamount_info.id
+     * 提现密码
+     * tbl_bankamount_info.forward_pwd
      */
-    public String getId(){
-        return id;
-    }
-
-    /**
-     * id
-     * tbl_bankamount_info.id
-     */
-    public void setId(String id){
-        this.id=id;
-    }
-
-    /**
-     * 用户名称
-     * tbl_bankamount_info.user_name
-     */
-    public String getUserName(){
-        return userName;
-    }
-
-    /**
-     * 用户名称
-     * tbl_bankamount_info.user_name
-     */
-    public void setUserName(String userName){
-        this.userName=userName;
-    }
-
-    /**
-     * 身份证号
-     * tbl_bankamount_info.idcard
-     */
-    public String getIdcard(){
-        return idcard;
-    }
-
-    /**
-     * 身份证号
-     * tbl_bankamount_info.idcard
-     */
-    public void setIdcard(String idcard){
-        this.idcard=idcard;
-    }
-
-    /**
-     * 电话
-     * tbl_bankamount_info.phone
-     */
-    public String getPhone(){
-        return phone;
-    }
-
-    /**
-     * 电话
-     * tbl_bankamount_info.phone
-     */
-    public void setPhone(String phone){
-        this.phone=phone;
-    }
-
-    /**
-     * 微信用户名称
-     * tbl_bankamount_info.wx_name
-     */
-    public String getWxName(){
-        return wxName;
-    }
-
-    /**
-     * 微信用户名称
-     * tbl_bankamount_info.wx_name
-     */
-    public void setWxName(String wxName){
-        this.wxName=wxName;
-    }
-
-    /**
-     * openid
-     * tbl_bankamount_info.openid
-     */
-    public String getOpenid(){
-        return openid;
-    }
-
-    /**
-     * openid
-     * tbl_bankamount_info.openid
-     */
-    public void setOpenid(String openid){
-        this.openid=openid;
-    }
-
-    /**
-     * 银行账户
-     * tbl_bankamount_info.bank_amount_no
-     */
-    public String getBankAmountNo(){
-        return bankAmountNo;
-    }
-
-    /**
-     * 银行账户
-     * tbl_bankamount_info.bank_amount_no
-     */
-    public void setBankAmountNo(String bankAmountNo){
-        this.bankAmountNo=bankAmountNo;
-    }
-
-    /**
-     * 银行名称
-     * tbl_bankamount_info.bank_name
-     */
-    public String getBankName(){
-        return bankName;
-    }
-
-    /**
-     * 银行名称
-     * tbl_bankamount_info.bank_name
-     */
-    public void setBankName(String bankName){
-        this.bankName=bankName;
-    }
-
-    /**
-     * 开户地址
-     * tbl_bankamount_info.bank_adress
-     */
-    public String getBankAdress(){
-        return bankAdress;
-    }
-
-    /**
-     * 开户地址
-     * tbl_bankamount_info.bank_adress
-     */
-    public void setBankAdress(String bankAdress){
-        this.bankAdress=bankAdress;
-    }
-
-    /**
-     * 创建时间
-     * tbl_bankamount_info.create_time
-     */
-    public String getCreateTime(){
-        return createTime;
-    }
-
-    /**
-     * 创建时间
-     * tbl_bankamount_info.create_time
-     */
-    public void setCreateTime(String createTime){
-        this.createTime=createTime;
-    }
-
-    /**
-     * 创建人
-     * tbl_bankamount_info.create_user
-     */
-    public String getCreateUser(){
-        return createUser;
-    }
-
-    /**
-     * 创建人
-     * tbl_bankamount_info.create_user
-     */
-    public void setCreateUser(String createUser){
-        this.createUser=createUser;
-    }
-
-    /**
-     * 修改时间
-     * tbl_bankamount_info.update_time
-     */
-    public String getUpdateTime(){
-        return updateTime;
-    }
-
-    /**
-     * 修改时间
-     * tbl_bankamount_info.update_time
-     */
-    public void setUpdateTime(String updateTime){
-        this.updateTime=updateTime;
-    }
-
-    /**
-     * 修改人
-     * tbl_bankamount_info.update_user
-     */
-    public String getUpdateUser(){
-        return updateUser;
-    }
-
-    /**
-     * 修改人
-     * tbl_bankamount_info.update_user
-     */
-    public void setUpdateUser(String updateUser){
-        this.updateUser=updateUser;
-    }
+    private String forwardPwd;
 
     /**
      * 自定义列，分页查询用
@@ -329,15 +110,123 @@ public class TblBankamountInfo implements java.io.Serializable{
     public void setLimitSize(Integer limitSize){
         this.limitSize = limitSize;
     }
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getIdcard() {
+		return idcard;
+	}
+	public void setIdcard(String idcard) {
+		this.idcard = idcard;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getWxName() {
+		return wxName;
+	}
+	public void setWxName(String wxName) {
+		this.wxName = wxName;
+	}
+	public String getOpenid() {
+		return openid;
+	}
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+	public String getBankAmountNo() {
+		return bankAmountNo;
+	}
+	public void setBankAmountNo(String bankAmountNo) {
+		this.bankAmountNo = bankAmountNo;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public String getBankAdress() {
+		return bankAdress;
+	}
+	public void setBankAdress(String bankAdress) {
+		this.bankAdress = bankAdress;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public String getForwardPwd() {
+		return forwardPwd;
+	}
+	public void setForwardPwd(String forwardPwd) {
+		this.forwardPwd = forwardPwd;
+	}
+	public TblBankamountInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public TblBankamountInfo(String id, String userName, String idcard, String phone, String wxName, String openid,
+			String bankAmountNo, String bankName, String bankAdress, String createTime, String createUser,
+			String updateTime, String updateUser, String forwardPwd, Integer limitStart, Integer limitSize) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.idcard = idcard;
+		this.phone = phone;
+		this.wxName = wxName;
+		this.openid = openid;
+		this.bankAmountNo = bankAmountNo;
+		this.bankName = bankName;
+		this.bankAdress = bankAdress;
+		this.createTime = createTime;
+		this.createUser = createUser;
+		this.updateTime = updateTime;
+		this.updateUser = updateUser;
+		this.forwardPwd = forwardPwd;
+		this.limitStart = limitStart;
+		this.limitSize = limitSize;
+	}
 	@Override
 	public String toString() {
 		return "TblBankamountInfo [id=" + id + ", userName=" + userName + ", idcard=" + idcard + ", phone=" + phone
 				+ ", wxName=" + wxName + ", openid=" + openid + ", bankAmountNo=" + bankAmountNo + ", bankName="
 				+ bankName + ", bankAdress=" + bankAdress + ", createTime=" + createTime + ", createUser=" + createUser
-				+ ", updateTime=" + updateTime + ", updateUser=" + updateUser + ", limitStart=" + limitStart
-				+ ", limitSize=" + limitSize + "]";
+				+ ", updateTime=" + updateTime + ", updateUser=" + updateUser + ", forwardPwd=" + forwardPwd
+				+ ", limitStart=" + limitStart + ", limitSize=" + limitSize + "]";
 	}
 	
-    
 }
 
